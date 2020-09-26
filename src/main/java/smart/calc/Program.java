3e55f9b41cccc610ac77261d213fa7b0f6d018e6
leks <kurbakov.av@gmail.com>
2020-09-26 16:33:58 +0300

    add: Program main loop

---
+++ src/main/java/smart/calc/Program.java
@@ -0,0 +1,61 @@
+package smart.calc;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+
+    private final Scanner scanner;
+
+    private final OutputStream out;
+
+    public Program() {
+        this(System.in, System.out);
+    }
+
+    public Program(InputStream in) {
+        this(in, System.out);
+    }
+
+    public Program(OutputStream out) {
+        this(System.in, out);
+    }
+
+    public Program(InputStream in, OutputStream out) {
+        this.scanner = new Scanner(in);
+        this.out = out;
+    }
+
+    private boolean enabled;
+
+    public void start() {
+        if (enabled) {
+            throw new IllegalStateException("Program already started");
+        }
+
+        enabled = true;
+        Calculator calculator = new Calculator();
+        while (enabled && scanner.hasNextLine()) {
+            try {
+                String line = scanner.nextLine();
+                Parser parser = new Parser(line);
+                List<String> statements = parser.parse();
+                BigDecimal result = calculator.calculate(statements);
+
+                out.write(result.toString().getBytes());
+                out.write("\n".getBytes());
+                out.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void shutdown() {
+        enabled = false;
+        scanner.close();
+    }
+}
