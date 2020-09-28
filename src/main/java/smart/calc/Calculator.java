9a86d26d0459e4d3ebb90e9a7c9205f5e90f2da2
leks <kurbakov.av@gmail.com>
2020-09-28 21:15:19 +0300

    refactoring: add default scale / overload calculate

--- src/main/java/smart/calc/Calculator.java
+++ src/main/java/smart/calc/Calculator.java
@@ -1,25 +1,35 @@
 package smart.calc;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.List;
 
 public class Calculator {
 
-    public BigDecimal calculate(List<String> statements) {
+    private static final int DEFAULT_SCALE = 2;
+
+    public BigDecimal calculate(String line) {
+        return calculate(line, DEFAULT_SCALE);
+    }
+
+    public BigDecimal calculate(String line, int scale) {
+        Parser parser = new Parser(line);
+        List<String> statements = parser.parse();
+
         Deque<BigDecimal> operands = new ArrayDeque<>();
         for (String statement : statements) {
             if ("+-*/".contains(statement) && operands.size() > 1) {
                 BigDecimal b = operands.pollLast();
                 BigDecimal a = operands.pollLast();
-                BigDecimal value = Operation.valueOf(statement).execute(a, b);
+                BigDecimal value = Operation.valueOf(statement).execute(a, b).setScale(16, RoundingMode.FLOOR);
                 operands.offer(value);
             } else {
-                operands.offer(new BigDecimal(statement));
+                operands.offer(new BigDecimal(statement).setScale(16, RoundingMode.FLOOR));
             }
         }
 
-        return operands.poll();
+        return operands.pop().setScale(scale, RoundingMode.FLOOR);
     }
 }
